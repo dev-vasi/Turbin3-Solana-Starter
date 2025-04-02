9926e6efb2aebcf3bd92e21c23af6e7e929e702f
Vasiulla <idofsyedvasi@gmail.com>
2025-04-02 20:35:41 +0530

    Done metadata and transfer

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,5 +1,11 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "../../Turbin3-wallet.json";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,19 +16,44 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("28t2kUraauxhmF1GETaXRFMJN2fTsMbvM9dtdtzEPdMR");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("BBWpMG3mXtGVMNVzGJSAVjkKqVixMXepWELv3fBL1RtU");
 
 (async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
+  try {
+    // Get the token account of the fromWallet address, and if it does not exist, create it
 
-        // Get the token account of the toWallet address, and if it does not exist, create it
+    let fromAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
 
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    // Get the token account of the toWallet address, and if it does not exist, create it
+
+    let toAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      to
+    );
+
+    // Transfer the new token to the "toTokenAccount" we just created
+
+    const transferSig = await transfer(
+      connection,
+      keypair,
+      fromAccount.address,
+      toAccount.address,
+      keypair.publicKey,
+      Number(1_000_000n)
+    );
+
+    console.log(`Your transfer txid: ${transferSig}`);
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
